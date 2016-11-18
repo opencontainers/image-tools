wking/image-spec

specs-go: Add ImageLayoutVersion and check oci-layout in tar engines
Collect the shared stuff in the image/layout utility package.

Signed-off-by: W. Trevor King <wking@tremily.us>

image/cas/layout/tar.go
@@ -24,6 +24,7 @@ import (
 	"strings"
 
 	"github.com/opencontainers/image-spec/image/cas"
+	"github.com/opencontainers/image-spec/image/layout"
 	"golang.org/x/net/context"
 )
 
@@ -33,9 +34,14 @@ type TarEngine struct {
 }
 
 // NewTarEngine returns a new TarEngine.
-func NewTarEngine(file ReadWriteSeekCloser) (engine cas.Engine, err error) {
-	engine = &TarEngine{
+func NewTarEngine(ctx context.Context, file ReadWriteSeekCloser) (eng cas.Engine, err error) {
+	engine := &TarEngine{
 		file: file,
+	}
+
+	err = layout.CheckTarVersion(ctx, engine.file)
+	if err != nil {
+		return nil, err
 	}
 
 	return engine, nil

image/refs/layout/tar.go
@@ -24,6 +24,7 @@ import (
 	"strings"
 
 	casLayout "github.com/opencontainers/image-spec/image/cas/layout"
+	imageLayout "github.com/opencontainers/image-spec/image/layout"
 	"github.com/opencontainers/image-spec/image/refs"
 	"github.com/opencontainers/image-spec/specs-go"
 	"golang.org/x/net/context"
@@ -35,9 +36,14 @@ type TarEngine struct {
 }
 
 // NewTarEngine returns a new TarEngine.
-func NewTarEngine(file casLayout.ReadSeekCloser) (engine refs.Engine, err error) {
-	engine = &TarEngine{
+func NewTarEngine(ctx context.Context, file casLayout.ReadWriteSeekCloser) (eng refs.Engine, err error) {
+	engine := &TarEngine{
 		file: file,
+	}
+
+	err = imageLayout.CheckTarVersion(ctx, engine.file)
+	if err != nil {
+		return nil, err
 	}
 
 	return engine, nil

opencontainers/image-tools

image/cas/layout/tar.go
@@ -22,6 +22,7 @@ import (
 	"os"
 
 	"github.com/opencontainers/image-tools/image/cas"
+	"github.com/opencontainers/image-tools/image/layout"
 	"golang.org/x/net/context"
 )
 
@@ -31,9 +32,14 @@ type TarEngine struct {
 }
 
 // NewTarEngine returns a new TarEngine.
-func NewTarEngine(file ReadWriteSeekCloser) (engine cas.Engine, err error) {
-	engine = &TarEngine{
+func NewTarEngine(ctx context.Context, file ReadWriteSeekCloser) (eng cas.Engine, err error) {
+	engine := &TarEngine{
 		file: file,
+	}
+
+	err = layout.CheckTarVersion(ctx, engine.file)
+	if err != nil {
+		return nil, err
 	}
 
 	return engine, nil

image/refs/layout/tar.go
@@ -24,6 +24,7 @@ import (
 
 	"github.com/opencontainers/image-spec/specs-go"
 	caslayout "github.com/opencontainers/image-tools/image/cas/layout"
+	imagelayout "github.com/opencontainers/image-tools/image/layout"
 	"github.com/opencontainers/image-tools/image/refs"
 	"golang.org/x/net/context"
 )
@@ -34,9 +35,14 @@ type TarEngine struct {
 }
 
 // NewTarEngine returns a new TarEngine.
-func NewTarEngine(file caslayout.ReadWriteSeekCloser) (engine refs.Engine, err error) {
-	engine = &TarEngine{
+func NewTarEngine(ctx context.Context, file caslayout.ReadWriteSeekCloser) (eng refs.Engine, err error) {
+	engine := &TarEngine{
 		file: file,
+	}
+
+	err = imagelayout.CheckTarVersion(ctx, engine.file)
+	if err != nil {
+		return nil, err
 	}
 
 	return engine, nil